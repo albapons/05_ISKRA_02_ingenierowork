9ba157c2d5a9f395a85c0a7f93bb16d94c42f265
albapons <albaponslloret@gmail.com>
2020-10-19 14:03:03 +0200

    working on pagination

--- client/src/components/ofertasPage/ofertas.js
+++ client/src/components/ofertasPage/ofertas.js
@@ -77,45 +77,6 @@ export default function Ofertas({ filtroSectores, filtroContratos }) {
           </div>
 
           <Paginate newBaseDades={newBaseDades} />
-
-          {/* {newBaseDades.map((e) => (
-            <div className="oferta" key={e.id}>
-              <div className="row">
-                <div className="col">
-                  <div className="title3 blue">{e.puesto}</div>
-                  <div className="font-weight-bold subtitle2">{e.lugar}</div>
-                </div>
-                <div className="col d-flex align-items-center justify-content-end">
-                  <button className="buttonBsmall">VER OFERTA</button>
-                </div>
-              </div>
-              <div className="row opciones subtitle2 px-4 pt-3">
-                <div className="my-2">
-                  <i className="far fa-calendar-alt mr-2" />
-                  Publicada:
-                  <strong>
-                    {" "}
-                    {e.publicación.split("-").reverse().join("-")}
-                  </strong>
-                </div>
-                <div className="my-2">
-                  <i className="far fa-calendar-alt mr-2" />
-                  Finaliza:
-                  <strong>
-                    {" "}
-                    {e.finalización.split("-").reverse().join("-")}
-                  </strong>
-                </div>
-                <div className="my-2">
-                  <i className="far fa-file-alt mr-2" /> Tipo de contrato:
-                  <strong> {e.contrato}</strong>
-                </div>
-              </div>
-              <div className="row p-4 subtitle1 text-justify">
-                {e.descripción}
-              </div>
-            </div>
-          ))} */}
         </div>
       </Router>
     </div>
--- client/src/components/ofertasPage/provaPaginate.js
+++ client/src/components/ofertasPage/provaPaginate.js
@@ -16,46 +16,76 @@ export default class provaPaginate extends Component {
     this.loadList();
   };
 
-  //! Carrega quines ofertes s'han de mostrar
+  // To refresh the props when they're ready
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.newBaseDades !== this.props.newBaseDades) {
+      this.setState({ currentPage: 1 });
+      this.loadList();
+    }
+    prevState.currentPage !== this.state.currentPage && this.loadList();
+  }
+
+  // Carrega quines ofertes s'han de mostrar
   loadList = () => {
-    console.log(this.props.newBaseDades); //! AIXÒ NO FUNCIONAAAAA!!!
     // Definir número de pàgines que tindrem
     this.setState({
       numberOfPages: Math.ceil(
         this.props.newBaseDades.length / this.state.numberPerPage
       ),
     });
-    //Definir origen i fi
+    //Definir origen i fi i filtrar
     var begin = (this.state.currentPage - 1) * this.state.numberPerPage;
     var end = begin + this.state.numberPerPage;
-    // Definir quines ofertes mostrem
     this.setState({ pageList: this.props.newBaseDades.slice(begin, end) });
     // this.check();       // determines the states of the pagination buttons
   };
 
-  //! Funcions per cada botó
-  // previousPage = () => {
-  //   this.state.currentPage -= 1;
-  //   this.loadList();
-  // };
+  // Display buttons
+  displayButtons = () => {
+    let buttons = [];
 
-  // firstPage = () => {
-  //   this.state.currentPage = 1;
-  //   this.loadList();
-  // };
+    for (let i = 1; i < this.state.numberOfPages; i++) {
+      buttons.push(
+        <button
+          className="buttonPagination"
+          id={i}
+          onClick={(e) => this.check(e)}
+        >
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  };
 
-  // lastPage = () => {
-  //   this.state.currentPage = this.state.numberOfPages;
-  //   this.loadList();
-  // };
+  //  Funcions per cada botó
+  previousPage = () => {
+    this.state.currentPage -= 1;
+    this.loadList();
+    //! actualitzar query amb nº de pàgina
+  };
+
+  nextPage = () => {
+    this.state.currentPage += 1;
+    this.loadList();
+    //! actualitzar query amb nº de pàgina
+  };
 
   //! Canvia l'estat dels botons
-  // check = () => {
-  // document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
-  // document.getElementById("previous").disabled = currentPage == 1 ? true : false;
-  // document.getElementById("first").disabled = currentPage == 1 ? true : false;
-  // document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
-  // };
+  check = (e) => {
+    const { currentPage, numberOfPages } = this.state;
+    // console.log(e.target.id);
+    this.setState({ currentPage: e.target.id });
+    this.loadList();
+
+    // Si currentPage === 1 botó previous disabled
+    document.getElementById(e.target.id).disabled =
+      currentPage == 1 ? true : false;
+    // Si currentPage === numberOfPages botó next disabled
+    document.getElementById(e.target.id).disabled =
+      currentPage == numberOfPages ? true : false;
+    //! Si currentPage === e.target.id aplicar styling botó actiu
+  };
 
   render() {
     const { newBaseDades } = this.props;
@@ -65,11 +95,11 @@ export default class provaPaginate extends Component {
       <div>
         {console.log(pageList)}
 
-        {/* {pageList.map((e) => ( */}
-        {newBaseDades.map((e) => (
+        {pageList.map((e) => (
           <div className="oferta" key={e.id}>
             <div className="row">
               <div className="col">
+                <div className="title3 blue">{e.id}</div>
                 <div className="title3 blue">{e.puesto}</div>
                 <div className="font-weight-bold subtitle2">{e.lugar}</div>
               </div>
@@ -101,14 +131,37 @@ export default class provaPaginate extends Component {
             </div>
           </div>
         ))}
-        {/* <div style="text-align:center;">
-     <input type="button" id="first" onclick="firstPage()" value="first" />
-     <input type="button" id="next" onclick="nextPage()" value="next" />
-     <input type="button" id="previous" onclick="previousPage()" value="previous" />
-     <input type="button" id="last" onclick="lastPage()" value="last" />
 
-     <div id="list"></div>
-     </div> */}
+        <div className="buttonsPagination">
+          <span onClick={(e) => this.previousPage(e)}>
+            <i className="fas fa-caret-left blue fa-3x mr-4" />
+          </span>
+          {this.displayButtons()}
+          {/* <button
+            className="buttonPagination"
+            id="1"
+            onClick={(e) => this.check(e)}
+          >
+            1
+          </button>
+          <button
+            className="buttonPagination"
+            id="2"
+            onClick={(e) => this.check(e)}
+          >
+            2
+          </button>
+          <button
+            className="buttonPagination"
+            id="3"
+            onClick={(e) => this.check(e)}
+          >
+            3
+          </button> */}
+          <span onClick={(e) => this.nextPage(e)}>
+            <i className="fas fa-caret-right blue fa-3x ml-4" />
+          </span>
+        </div>
       </div>
     );
   }
